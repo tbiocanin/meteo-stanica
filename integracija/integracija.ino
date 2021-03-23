dc378197787afe84d9b73ef0dddd7571e11baf3d
Teodor <tekikralj011@gmail.com>
2021-03-24 00:18:13 +0100

    dve funkcije apdejtovane

---
+++ integracija/integracija.ino
@@ -0,0 +1,65 @@
+int zelenaLedSv = 2;
+int plavaLed = 3;
+int zutaLed = 4;
+int crvenaLed = 5;
+float tempUlaz;
+int tempAnalogniPin = A5;
+int sensorPin = A4; //analogni ulaz na kom se nalazi fotootpornik 
+int photoCellIn = 0; //ulazna vrednost fotootpornika
+
+float temperaturaF(int pin) {
+  float tempVrednost = analogRead(pin); //ucitaj vrednost sa analognog ulaza
+  
+  float napon = (tempVrednost/1024.0) * 5.0;
+  float temperatura = (napon - .5) * 100;
+ 
+  return temperatura;
+}
+
+void toggle(int pin){
+
+  int ledStateOff = 0; //ugasena led 
+  if(digitalRead(pin) == 1) { //proveri da li led radi
+   digitalWrite(pin, ledStateOff); //ako radi, ugasi je
+  }
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  for(int led = 2; led < 6; led++){
+   pinMode(led, OUTPUT);
+    
+  }
+  
+}
+
+void loop()
+{
+  
+  float temperatura = temperaturaF(tempAnalogniPin);
+
+  if(temperatura >-50 && temperatura < 15) {
+    for(int led = 3; led < 6; led ++){
+     toggle(led);
+    }
+    digitalWrite(plavaLed, HIGH);
+  } else if (temperatura > 16 && temperatura < 30) {
+    for(int led = 3; led < 6; led ++){
+     toggle(led);
+    }
+    digitalWrite(zutaLed, HIGH);
+  } else { 
+    for(int led = 3; led < 6; led ++){
+     toggle(led);
+    }
+    digitalWrite(crvenaLed, HIGH);
+  }
+  
+  
+  photoCellIn = analogRead(sensorPin)/4; //reskaliranje na interval od 0-255 zbog LED sijalice
+  analogWrite(zelenaLedSv, photoCellIn); //za vrednost koju je fotootpornik pokupio, sijalica ce da svetli
+  
+  
+}
